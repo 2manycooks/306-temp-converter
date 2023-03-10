d8bb2ee047cfce7ff1cd19faee510cc932adf160
2manycooks <73556731+2manycooks@users.noreply.github.com>
2023-03-10 10:05:20 -0800

    lesson done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello, front end');
+let input = document.querySelector(".input")
+let fToC = document.querySelector(".ftoc")
+let cToF = document.querySelector(".ctof")
+let submit = document.querySelector(".submit-btn")
+let clear = document.querySelector(".clear-btn")
+let tempArea = document.querySelector(".temp-area")
+
+let fConversion = function() {
+    let preConversionValue = input.value
+    let postConversionValue = (preConversionValue - 32) * (5/9)
+    tempArea.innerText = postConversionValue
+}
+
+let cConversion = function() {
+    let preConversionValue = input.value
+    let postConversionValue = (preConversionValue * 9/5) + 32
+    tempArea.innerText = postConversionValue
+}
+
+let clearText = function() {
+    tempArea.innerText = ""
+}
+
+fToC.addEventListener("click", fConversion)
+cToF.addEventListener("click", cConversion)
+clear.addEventListener("click", clearText)

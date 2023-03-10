c94d2f0953026a4d6167924dc9b963cc986158ec
2manycooks <73556731+2manycooks@users.noreply.github.com>
2023-03-10 10:14:22 -0800

    changes colors

--- js/script.js
+++ js/script.js
@@ -9,12 +9,24 @@ let fConversion = function() {
     let preConversionValue = input.value
     let postConversionValue = (preConversionValue - 32) * (5/9)
     tempArea.innerText = postConversionValue
+
+    if(postConversionValue < 0) {
+        tempArea.style.backgroundColor = "blue"
+    } else if (postConversionValue > 50) {
+        tempArea.style.backgroundColor = "red"
+    }
 }
 
 let cConversion = function() {
     let preConversionValue = input.value
     let postConversionValue = (preConversionValue * 9/5) + 32
     tempArea.innerText = postConversionValue
+
+    if(postConversionValue < 32) {
+        tempArea.style.backgroundColor = "blue"
+    } else if (postConversionValue > 120) {
+        tempArea.style.backgroundColor = "red"
+    }
 }
 
 let clearText = function() {
